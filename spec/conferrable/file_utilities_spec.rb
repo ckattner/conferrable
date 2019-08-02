5ba765e5d9db78fb2b04e0a82c2b46710153fd3a
Ryan Gerry <rgerry@bluemarblepayroll.com>
2019-08-02 14:39:06 -0500

    Guard should run tests when a production file is saved.

--- spec/conferrable/file_utilities_spec.rb
+++ spec/conferrable/file_utilities_spec.rb
@@ -12,14 +12,14 @@ require './spec/spec_helper'
 describe Conferrable::FileUtilities do
   describe 'reading YAML files' do
     it 'returns a parsed YAML and ERB evaluated result for a *.yml.erb file' do
-      file_path = File.expand_path('config_examples/simple.yml.erb', __dir__)
+      file_path = File.expand_path('../config_examples/simple.yml.erb', __dir__)
       expected_result = { 'key1' => %w[static_element dynamic_element] }
 
       expect(described_class.read(file_path)).to eq(expected_result)
     end
 
     it 'returns a parsed YAML evaluated result for a *.yml file' do
-      file_path = File.expand_path('config_examples/simple.yml', __dir__)
+      file_path = File.expand_path('../config_examples/simple.yml', __dir__)
       expected_result = { 'key1' => ['element1', '<%= "ERB which will not evaluate" =>'] }
 
       expect(described_class.read(file_path)).to eq(expected_result)
